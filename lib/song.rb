dc58ec8dd7d4af020da568e845b283df2b63d687
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-07 18:01:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,11 +12,18 @@ class Song
     song
   end
 
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
   def self.create_by_name(name)
     song = self.new
     song.name = name
     @@all << song
     song
+  end
 
   def save
     self.class.all << self
